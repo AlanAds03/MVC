8fe340f08040e9efa4c50b9c7f8036e0e3fe2bf0
AlanAds03 <alan.ads03@gmail.com>
2015-07-21 02:20:07 -0300

    Projeto Vocabulario

---
+++ Vocabulario/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vocabulario.Startup))]
+namespace Vocabulario
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
